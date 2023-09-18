bd57285f3bdba646bca56993315a6e74a27c2c39
Stephan Griesel <stephangriesel@peopleintouch.com>
2023-09-18 07:23:03 +0200

    refactor & routes

--- main.go
+++ main.go
@@ -1,27 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"net/http"
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	"github.com/dreamsofcode-io/orders-api/application"
 )
 
 func main(){
-	router := chi.NewRouter()
-	router.Use(middleware.Logger)
-
-	router.Get("/hello", basicHandler)
-	server := &http.Server{
-		Addr: ":3000",
-		Handler: router,
-	}
-	err := server.ListenAndServe()
+	app := application.New()
+	err := app.Start(context.TODO())
 	if err != nil {
-		fmt.Println("failed to listen to server", err)
+		fmt.Println("❌ failed to start app:",err)
 	}
-}
-
-func basicHandler(w http.ResponseWriter, r *http.Request){
-	w.Write([]byte("Hello world"))
 }
