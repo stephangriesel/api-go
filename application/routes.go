1d7e8791aca97e9468bec64b21e2f750227b1977
Stephan Griesel <stephangriesel@peopleintouch.com>
2023-09-20 10:28:19 +0200

    handler methods

--- application/routes.go
+++ application/routes.go
@@ -3,13 +3,13 @@ package application
 import (
 	"net/http"
 
+	"github.com/dreamsofcode-io/orders-api/handler"
+	"github.com/dreamsofcode-io/orders-api/repository/order"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
-
-	"github.com/dreamsofcode-io/orders-api/handler"
 )
 
-func loadRoutes() *chi.Mux {
+func (a *App) loadRoutes() {
 	router := chi.NewRouter()
 
 	router.Use(middleware.Logger)
@@ -18,17 +18,21 @@ func loadRoutes() *chi.Mux {
 		w.WriteHeader(http.StatusOK)
 	})
 
-	router.Route("/orders", loadOrderRoutes)
+	router.Route("/orders", a.loadOrderRoutes)
 
-	return router
+	a.router = router
 }
 
-func loadOrderRoutes(router chi.Router) {
-	orderHandler := &handler.Order{}
+func (a *App) loadOrderRoutes(router chi.Router) {
+	orderHandler := &handler.Order{
+		Repo: &order.RedisRepo{
+			Client: a.rdb,
+		},
+	}
 
 	router.Post("/", orderHandler.Create)
 	router.Get("/", orderHandler.List)
 	router.Get("/{id}", orderHandler.GetByID)
 	router.Put("/{id}", orderHandler.UpdateByID)
-  router.Delete("/{id}", orderHandler.DeleteByID)
+	router.Delete("/{id}", orderHandler.DeleteByID)
 }
